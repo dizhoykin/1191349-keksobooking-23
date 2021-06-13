78758e56ccae2c5fea50205f52fbeccd0c04303e
dizhoykin <dizhoykin@gmail.com>
2021-06-13 14:26:00 +0700

    Модули.начало

--- js/main.js
+++ js/main.js
@@ -1,36 +1,43 @@
-// Скопировано с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-function getRandomIntInclusive(min, max) {
+// Функция генерации случайного целого числа из заданного диапазона
+const getRandomIntInclusive = (min, max) => {
   if (min < 0 || max < 0 || max < min) {
     throw new Error('Введен неверный диапазон чисел');
   }
   min = Math.ceil(min); // Округление левого интервала вверх, если введено число с плавающей точкой
   max = Math.floor(max); // Округление правого интервала вниз, если введено число с плавающей точкой
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-function getRandomFloatInclusive(min, max, decimal) {
+//Функция генерации случайного числа с плавающей точной из заданного диапазона с указанием числа знаков десятичной части
+const getRandomFloatInclusive = (min, max, decimal) => {
   if (min < 0 || max < 0 || max < min) {
     throw new Error('Введен неверный диапазон чисел');
   } else if (decimal < 0 || !Number.isInteger(decimal)) {
     throw new Error('Введено неверное число знаков после запятой');
   }
   return Number((Math.random() * (max - min) + min).toFixed(decimal));
-}
+};
+
+// Функция поиска случайного элемента в массиве
+const getRandomArrayElement = (array) => array[Math.floor(Math.random()*array.length)];
+
+// Функция генерации массива случайной длины с уникальными эелементами из исходного массива
+const createRandomLengthArray = (initialArray) => {
+  const randomArrayLength = getRandomIntInclusive(1, initialArray.length);
+  const randomLengthArray = [];
+  for (let index = 0; index < randomArrayLength; index++) {
+    const randomLengthArrayElement = getRandomArrayElement(initialArray);
+    randomLengthArray.push(randomLengthArrayElement);
+  }
+  const uniqueАrray = [...new Set(randomLengthArray)];
+  return uniqueАrray;
+};
 
 // Функция генерации объекта "Author"
 const createAuthor = (index) => {
-  let avatar = `img/avatars/user0${index}.png`;
-  if (index >= 10) {
-    avatar = `img/avatars/user${index}.png`;
-  }
+  const avatar = `img/avatars/user${  String(index).padStart(2,'0')  }.png`;
   return avatar;
 };
-
-// const createAuthor = (index) => {
-//   const avatar = `${'img/avatars/user'.padEnd(18,`0${index}`)}.png`;
-//   return avatar;
-// };
 
 // Массивы значений ключей объектов "Offer"
 const TITLE = ['Удобное жилье', 'Лучшее расположение', 'Отличное место', 'Красивый вид', 'Спокойное проживание', 'Близко до метро'];
@@ -52,44 +59,22 @@ const MIN_LNG = 139.70000;
 const MAX_LNG = 139.80000;
 const DECIMAL = 5;
 
-// Функция-прототип для поиска случайного элемента в массиве
-// Array.prototype.sample = function() {
-//   return this[Math.floor(Math.random()*this.length)];
-// };
-
-const getRandomArrayElement = (array) => {
-  return array[Math.floor(Math.random()*array.length)];
-};
-
-// Функция генерации массива случайной длины с уникальными эелементами из исходного массива
-const createRandomLengthArray = (initialArray) => {
-  const randomArrayLength = getRandomIntInclusive(1, initialArray.length);
-  const randomLengthArray = [];
-  for (let index = 0; index < randomArrayLength; index++) {
-    const randomLengthArrayElement = getRandomArrayElement(initialArray);
-    randomLengthArray.push(randomLengthArrayElement);
-  }
-  const uniqueАrray = [...new Set(randomLengthArray)];
-  return uniqueАrray;
-};
-
 // Функция генерации объекта "Offer"
-const createOffer = (lat, lng) => {
-  return {
-    title: getRandomArrayElement(TITLE),
-    address: {lat, lng},
-    price: getRandomIntInclusive(1, 1000),
-    type: getRandomArrayElement(TYPE),
-    rooms: getRandomIntInclusive(1, 7),
-    guests: getRandomIntInclusive(1, 7),
-    checkin: getRandomArrayElement(TIME),
-    checkout: getRandomArrayElement(TIME),
-    features: createRandomLengthArray(FEATURES),
-    description: getRandomArrayElement(DESCRIPTION),
-    photos: createRandomLengthArray(PHOTOS),
-  };
-};
+const createOffer = (lat, lng) => ({
+  title: getRandomArrayElement(TITLE),
+  address: {lat, lng},
+  price: getRandomIntInclusive(1, 1000),
+  type: getRandomArrayElement(TYPE),
+  rooms: getRandomIntInclusive(1, 7),
+  guests: getRandomIntInclusive(1, 7),
+  checkin: getRandomArrayElement(TIME),
+  checkout: getRandomArrayElement(TIME),
+  features: createRandomLengthArray(FEATURES),
+  description: getRandomArrayElement(DESCRIPTION),
+  photos: createRandomLengthArray(PHOTOS),
+});
 
+// Функция генерации объявления
 const createAd = (index) => {
   const lat = getRandomFloatInclusive(MIN_LAT, MAX_LAT, DECIMAL);
   const lng = getRandomFloatInclusive(MIN_LNG, MAX_LNG, DECIMAL);
@@ -104,6 +89,7 @@ const createAd = (index) => {
   return ad;
 };
 
+// Функция генерации массива объявлений
 const createAdsArray = (adsNumber) => {
   const adsArray = [];
   for (let index = 1; index <= adsNumber; index++) {
