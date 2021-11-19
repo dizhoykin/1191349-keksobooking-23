6c47ab864e958c172c1c45fafa3908b5e9f980c9
dizhoykin <dizhoykin@gmail.com>
2021-11-19 17:31:15 +0700

    проверка проблемы с отображением svg-иконок

--- js/map.js
+++ js/map.js
@@ -145,7 +145,7 @@ const setAdsToMap = () => {
     .slice(0, ADS_COUNT)
     .forEach((adsListElement) => {
       const icon = L.icon({
-        iconUrl: '../img/pin.jpg',
+        iconUrl: 'img/pin.svg',
         iconSize: [40, 40],
         iconAnchor: [20, 40],
       });
